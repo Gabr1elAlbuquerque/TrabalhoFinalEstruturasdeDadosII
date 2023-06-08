239a734a10050df248f5ad258f7ae9b473e36a18
Gabriel Albuquerque <96128995+Gabr1elAlbuquerque@users.noreply.github.com>
2023-06-08 17:20:50 -0300

    Ler arquivo
    Função ler arquivo e converter str para int

--- ProjetoEDII/Funcoes.h
+++ ProjetoEDII/Funcoes.h
@@ -5,30 +5,27 @@
 #include <stdlib.h>
 #include <string.h>
 
-void LerArquivo(char *arquivo, int* k, int* n, char* numeros) {
+void LerArquivo(FILE *arquivo, int *k, int *n, char *numeros) {
     if (arquivo == NULL) {
         printf("Erro ao abrir o arquivo.\n");
         return;
     }
+
     fscanf(arquivo, "%d %d %s", k, n, numeros);
-    fclose(arquivo);
 }
 
-void ConverterStrInt(char *numeros, int *valores, int *cont){
-  
-  char *token;
-  int contador = 0;
-  cont = 0;
+void ConverterStrInt(char *numeros, int *valores, int *cont) {
+    char *token;
+    int contador = 0;
+    *cont = 0;
 
-  token = strtok(numeros, ";");
-  while(token != NULL){
-    valores[contador] = atoi(token);
-    contador++;
-    *cont = *cont + 1;
-    token = strtok(NULL, ";");
-  }
+    token = strtok(numeros, ";");
+    while (token != NULL) {
+        valores[contador] = atoi(token);
+        contador++;
+        *cont = contador;
+        token = strtok(NULL, ";");
+    }
 }
-
-
 
 #endif
--- ProjetoEDII/Main.c
+++ ProjetoEDII/Main.c
@@ -1,25 +1,21 @@
-#include <stdio.h>
-#include <string.h>
-#include "funcoes.h"
+#include "Funcoes.h"
+int main(int argc, char *argv[]) {
+    char numeros[100];
+    int k, n, valores[100], cont = 0;
 
-int main(int argc, char **argv) {
-  
-char numeros[100];
-int k, n, valores[100], *cont = 0;
+    FILE *arquivo = fopen(argv[1], "r");
+    LerArquivo(arquivo, &k, &n, numeros);
+    fclose(arquivo);
 
-FILE* arquivo = fopen(argv[1], "r");
-LerArquivo(&arquivo, &n, numeros);
-  
-printf("%d\n", k);
-printf("%d\n", n);
-printf("%s\n", numeros);
+    printf("%d\n", k);
+    printf("%d\n", n);
+    printf("%s\n", numeros);
 
-ConverterStrInt(numeros, valores, cont);
+    ConverterStrInt(numeros, valores, &cont);
 
-for(int i = 0; i < *cont; i++){
-  printf("%d\n", valores[*cont]);
-}
+    for (int i = 0; i < cont; i++) {
+        printf("%d\n", valores[i]);
+    }
 
-return 0;
-  
+    return 0;
 }
